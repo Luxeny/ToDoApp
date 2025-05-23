73121d2490ca0a0e8214d2a5b1ebe35583c2558d
nitta <nitta@DESKTOP-IE2HGGK>
2025-05-23 22:32:25 +0700

    реализация паттерна Command

---
+++ ToDoApp/Commands/AddTaskCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace ToDoApp.Commands
+{
+    public class AddTaskCommand : ICommand
+    {
+        private readonly CheckedListBox _listBox;
+        private readonly string _task;
+
+        public AddTaskCommand(CheckedListBox listBox, string task)
+        {
+            _listBox = listBox;
+            _task = task;
+        }
+
+        public void Execute() => _listBox.Items.Add(_task);
+        public void Undo() => _listBox.Items.Remove(_task);
+    }
+}
